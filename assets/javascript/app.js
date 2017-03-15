2ab8d7513c7583891a4697627a25f2ca4cb447fc
rebecca palmore <rpalmore@wttw.com>
2017-03-15 11:00:44 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Adding in button hover elements
+$("button").hover(function(){
+    $(this).css("background-color", "#18c8e4");
+    }, function(){
+    $(this).css("background-color", "white");
+ });
+
+ // And some fun link hover decoration
+$("a").hover(function(){
+    $(this).css("background-color", "#18c8e4");
+     }, function(){
+    $(this).css("background-color", "white");
+ });
